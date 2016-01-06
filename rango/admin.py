6e5748f2837e4f16570581a3611ab01d8dbb15ee
mikejguo290 <mikejguo290@gmail.com>
2016-01-06 22:31:21 +0000

    Create PageAdmin in rango/admin.py to list pages with fields (name, category, url) in the admin panel.

--- rango/admin.py
+++ rango/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from rango.models import Category, Page
 from rango.models import UserProfile # new model created to add functionality to auth User
 
+class PageAdmin(admin.ModelAdmin):
+	"""
+	Add a new admin object to format Page model output.
+	"""
+	list_display = ('title', 'category', 'url')
+
 # make Category, Page models available to the admin interface.
 
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin) # admin to register Page model together with PageAdmin admin obj
 admin.site.register(UserProfile)
-
+	
